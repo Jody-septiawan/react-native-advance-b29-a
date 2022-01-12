9960d2d1e62fa654e2d940a69923def27df8a191
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-12 11:14:07 +0700

    bottom tab navigation

--- container.js
+++ container.js
@@ -1,35 +1,63 @@
 // Import React Navigation & Stack
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+// Import Icon
+import { Ionicons } from '@expo/vector-icons'
 
 // Import Styling
 import { useTheme } from 'native-base'
 
-// Create Stack Navigation
+// Create Stack & Bottom Tab Navigation
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 // Import Screens
 import FormNativeBase from './src/screens/form'
 import Hello from './src/screens/hello'
 import IncDec from './src/screens/incDec'
 
-export default function Container(){
-
+function MyTab() {
     const theme = useTheme()
+    return (
+        <Tab.Navigator 
+            initialRouteName='Hello'
+            screenOptions={({ route }) => ({
+                headerMode: 'screen',
+                headerTintColor: 'white',
+                headerStyle: { backgroundColor: theme.colors.primary['900'] },
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
+                    if(route.name === "Hello"){
+                        iconName = focused ? "ios-home" : "ios-home-outline"
+                    } else if (route.name == "Form") {
+                        iconName = focused ? "ios-information-circle" : "ios-information-circle-outline"
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />
+                },
+                tabBarActiveTintColor: theme.colors.primary['600'],
+                tabBarInactiveTintColor: 'gray'
+            })}
+        >
+            <Tab.Screen name="Hello" component={Hello} />
+            <Tab.Screen name="Form" component={FormNativeBase} />
+        </Tab.Navigator>
+    )
+}
+
+export default function Container(){
     return (
         <NavigationContainer>
-            <Stack.Navigator 
-                initialRouteName='Home'
-                screenOptions={{
-                    headerStyle: { 
-                        backgroundColor: theme.colors.primary['900']
-                     }
-                }}
-            >
+            <Stack.Navigator>
                 <Stack.Screen 
                     name='Home'
-                    component={Hello} 
+                    component={MyTab} 
+                    options={{
+                        headerShown: false
+                    }}
                 />
                 <Stack.Screen 
                     name='IncDec'
---
+++ src/screens/detailSoc.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Text, Box } from "native-base";
+
+// Declare route
+export default function DetailSoc({ route }) {
+    const data = route.params.value
+  return (
+    <Box
+      safeArea
+      bg="primary.400"
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      p={10}
+    >
+      <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
+        {data}
+      </Text>
+    </Box>
+  );
+}
